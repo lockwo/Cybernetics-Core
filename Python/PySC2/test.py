39352132d264281d9eb9a10b267055127f702d78
owenlockwood2@outlook.com <owenlockwood2@outlook.com>
2019-02-19 17:01:51 -0500

    update

--- Python/PySC2/test.py
+++ Python/PySC2/test.py
@@ -1,1 +1,8 @@
-print("Hello World")
+from pysc2.agents import base_agent
+from pysc2.lib import actions
+
+class Simple(base_agent.BaseAgent):
+    def step(self, obs):
+        super(Simple, self).step(obs)
+        return actions.FunctionCall(actions.FUNCTIONS.no_op.id, [])
+        
