a491d0ca3761beaf8ed58a26bc6ed9f6afbe3d78
Owen Lockwood <42878312+lockwo@users.noreply.github.com>
2019-03-22 23:42:41 -0400

    Update zealot2.py

--- Python/PySC2/zealot2.py
+++ Python/PySC2/zealot2.py
@@ -7,8 +7,14 @@ from absl import app
 import random
 
 class P_Agent(base_agent.BaseAgent):
+    def __init__(self):
+        super(P_Agent, self).__init__()
+        self.attack_coordinates = None
+
+
     def get_units_by_type(self, obs, unit_type):
         return [unit for unit in obs.observation.feature_units if unit.unit_type == unit_type]
+    
     def unit_type_is_selected(self, obs, unit_type):
        if (len(obs.observation.single_select) > 0 and obs.observation.single_select[0].unit_type == unit_type):
            return True
@@ -21,7 +27,37 @@ class P_Agent(base_agent.BaseAgent):
     def step(self, obs):
         super(P_Agent, self).step(obs)
 
+        if obs.first():
+            player_y, player_x = (obs.observation.feature_minimap.player_relative == features.PlayerRelative.SELF).nonzero()
+
+            xmean = player_x.mean()
+            ymean = player_y.mean()
+
+            if xmean <= 31 and ymean <= 31:
+                self.attack_coordinates = (49, 49)
+            else:
+                self.attack_coordinates = (12, 16)
+
         gateway = self.get_units_by_type(obs, units.Protoss.Gateway)
+        pylon = self.get_units_by_type(obs, units.Protoss.Pylon)
+        zealots = self.get_units_by_type(obs, units.Protoss.Zealot)
+
+        if len(zealots) > 4:
+            if self.unit_type_is_selected(obs, units.Protoss.Zealot):
+                if (actions.FUNCTIONS.Attack_minimap.id):
+                    return actions.FUNCTIONS.Attack_minimap("now", self.attack_coordinates)
+
+            if (actions.FUNCTIONS.select_army.id in obs.observation.available_actions):
+                return actions.FUNCTIONS.select_army("select")
+
+        if len(pylon) == 0:
+            if self.unit_type_is_selected(obs, units.Protoss.Probe):
+                if (actions.FUNCTIONS.Build_Pylon_screen.id in obs.observation.available_actions):
+                    x = random.randint(0, 83)
+                    y = random.randint(0, 83)
+
+                    return actions.FUNCTIONS.Build_Pylon_screen("now", (x, y))
+        
         if len(gateway) == 0:
             if self.unit_type_is_selected(obs, units.Protoss.Probe):
                 if (actions.FUNCTIONS.Build_Gateway_screen.id in obs.observation.available_actions):
@@ -30,11 +66,19 @@ class P_Agent(base_agent.BaseAgent):
 
                     return actions.FUNCTIONS.Build_Gateway_screen("now", (x, y))
 
-        probes = get_units_by_type(obs, units.Protoss.Probe)
+        if len(gateway) == 1:
+            if self.unit_type_is_selected(obs, units.Protoss.Gateway):
+                if (actions.FUNCTIONS.Train_Zealot_quick.id in obs.observation.available_actions):
+                    return actions.FUNCTIONS.Train_Zealot_quick("now")
+            else:
+                return actions.FUNCTIONS.select_point("select_all_type", (gateway[0].x, gateway[0].y))
+
+        probes = self.get_units_by_type(obs, units.Protoss.Probe)
         if len(probes) > 0:
             probe = random.choice(probes)
             return actions.FUNCTIONS.select_point("select_all_type", (probe.x, probe.y))
 
+          
         return actions.FUNCTIONS.no_op()
 
     
